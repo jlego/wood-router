4c636689ce7f9d84d417bd4cd6b5e82d8198c7e7
yuronghui <yuronghui@singworld.net>
2018-11-19 19:08:36 +0800

    no message

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const Router = require('./src/router');
 const _routers = new Map();
 
 module.exports = (app, config = {}) => {
-  app.Router = function(controllerName){
+  let router = function(controllerName){
     if(_routers.has(controllerName)){
       return _routers.get(controllerName);
     } else {
       let _router = new Router(controllerName, app._controllers);
-      if(controllerName) app.Router.set(controllerName, _router);
+      if(controllerName) _routers.set(controllerName, _router);
       return _router;
     }
   }
-  app.application.use('/', app.Router().getRouter());
-  app.addProp('Router', app.Router);
-  return app.Router;
+  app.application.use('/', router().getRouter());
+  app.addAppProp('Router', router);
+  return router;
 }
