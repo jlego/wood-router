750b6d62cb21d4b417bd7d1a30be16db53c6641e
yuronghui <yuronghui@singworld.net>
2018-11-19 19:05:56 +0800

    no message

--- index.js
+++ index.js
@@ -3,18 +3,19 @@
  * by jlego on 2018-11-17
  */
 const Router = require('./src/router');
+const _routers = new Map();
 
-module.exports = app => {
-  app._routers = app._routers || new Map();
+module.exports = (app, config = {}) => {
   app.Router = function(controllerName){
-    if(app._routers.has(controllerName)){
-      return app._routers.get(controllerName);
+    if(_routers.has(controllerName)){
+      return _routers.get(controllerName);
     } else {
       let _router = new Router(controllerName, app._controllers);
-      if(controllerName) app._routers.set(controllerName, _router);
+      if(controllerName) app.Router.set(controllerName, _router);
       return _router;
     }
   }
   app.application.use('/', app.Router().getRouter());
+  app.addProp('Router', app.Router);
   return app.Router;
 }
