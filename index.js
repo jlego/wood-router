e82b18c2acd64ea736255925d20937b5c62c105d
jlego <273734893@qq.com>
2018-11-20 02:21:59 +0800

    no message

--- index.js
+++ index.js
@@ -3,19 +3,20 @@
  * by jlego on 2018-11-17
  */
 const Router = require('./src/router');
-const _routers = new Map();
 
 module.exports = (app, config = {}) => {
-  let router = function(controllerName){
+  const _routers = new Map();
+  let _controllers = app.Plugin('controller')._controllers;
+  app.Router = function(controllerName){
     if(_routers.has(controllerName)){
       return _routers.get(controllerName);
     } else {
-      let _router = new Router(controllerName, app._controllers);
+      let _router = new Router(controllerName, _controllers);
       if(controllerName) _routers.set(controllerName, _router);
       return _router;
     }
   }
-  app.application.use('/', router().getRouter());
-  app.addAppProp('Router', router);
-  return router;
+  app.application.use('/', app.Router().getRouter());
+  if(app.addAppProp) app.addAppProp('Router', app.Router);
+  return app;
 }
