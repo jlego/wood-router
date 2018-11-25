7e99a61dd5d595710a580da33c8639c8a1b49a91
jlego <273734893@qq.com>
2018-11-25 12:02:19 +0800

    no message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Router = require('./src/router');
 
 module.exports = (app, config = {}) => {
   const _routers = new Map();
-  let _controllers = app.getPlugin('controller')._controllers;
+  let _controllers = app.Plugin('controller')._controllers;
   app.Router = function(controllerName){
     if(_routers.has(controllerName)){
       return _routers.get(controllerName);
