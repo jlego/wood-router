3adcf846807e6fc037f79fedb9ca4c387c495f4a
jlego <273734893@qq.com>
2020-01-21 23:30:15 +0800

    no message

--- src/router.js
+++ src/router.js
@@ -13,16 +13,31 @@ class Router {
     return router;
   }
 
+  //捕捉async方法异常
+  async _asyncWarpFun(...args) {
+    const next = args[args.length-1];
+    try {
+      return Promise.resolve(this(...args)).catch(next);
+    } catch(e) {
+      next(e);
+    }
+  }
+
   _addRouter(method, argus){
     if(argus.length > 1){
+      for(let i of argus) {
+        if (!i) throw new Error(`路由:${argus[0]}下对应方法不存在,请检查`);
+      }
       let pathMatch = argus.shift(),
         fun = argus.pop(),
         methodArr = ['get', 'post', 'put', 'delete'];
       if(this.controllerName && this.controllers && typeof fun === 'function'){
         fun = fun.bind(this.controllers.get(this.controllerName));
       }
-      if(method === 'post') argus.unshift(multipart);
-      if(methodArr.includes(method)) router[method](pathMatch, ...argus, fun);
+      if(method === 'post') argus.unshift(multipart());
+      if(methodArr.includes(method)) router[method](pathMatch, ...argus, this._asyncWarpFun.bind(fun));
+    } else {
+      throw new Error('没有实现路由对应的方法')
     }
   }
 
